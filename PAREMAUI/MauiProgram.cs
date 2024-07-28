aaef5c0688943c193df86b141b1f6ba8aa5b4eae
Cristian C. Giraldo <cristiansiempre.net@hotmail.com>
2024-07-27 22:44:00 -0500

    Añadir nuevos proyectos y mejorar autenticación
    Se han añadido los proyectos "PARE.Models" y "PARE.WebApiClient" al archivo de solución `PAREAPP.sln`, junto con sus configuraciones de compilación y despliegue.
    
    En `MainLayout.razor`, se ha actualizado la invocación de `Js.InvokeVoidAsync` a `_jsRuntime.InvokeVoidAsync`.
    
    En `Routes.razor`, se ha reemplazado el componente `Router` por `CascadingAuthenticationState` y `AuthorizeRouteView` para manejar la autenticación y autorización de rutas.
    
    En `_Imports.razor`, se han añadido directivas `@using` y se han actualizado las inyecciones de dependencias.
    
    En `MauiProgram.cs`, se han añadido servicios de autenticación, estado de la aplicación y servicios API, así como referencias a los nuevos proyectos.
    
    En `PAREMAUI.csproj`, se han añadido referencias a paquetes necesarios para la autenticación y servicios API, y se han añadido referencias a los nuevos proyectos.
    
    Se han añadido los archivos `EmptyLayout.razor`, `RedirectToLogin.razor`, `RedirectToLogin.razor.cs`, `AppState.cs`, `AuthService.cs` y `CustomAuthStateProvider.cs` para manejar la autenticación y el estado de la aplicación.

--- PAREMAUI/MauiProgram.cs
+++ PAREMAUI/MauiProgram.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
+using PARE.WebApiClient;
+using PAREMAUI.Essentials;
+using PAREMAUI.Providers;
 
 namespace PAREMAUI
 {
@@ -21,6 +25,23 @@ namespace PAREMAUI
             builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
+            builder.Services.AddMudServices();
+            //AUTH
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<CustomAuthStateProvider>();
+            builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomAuthStateProvider>());
+
+            //Essentials
+            builder.Services.AddScoped<AuthService>();
+            builder.Services.AddScoped<AppState>();
+
+            //Client Web
+            builder.Services.AddScoped<Client>();
+
+            //Api Services
+            builder.Services.AddScoped<UsersApiService>();
+            builder.Services.AddScoped<CitiesApiService>();
+
             builder.Services.AddMudServices(config =>
             {
                 config.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.BottomCenter;
@@ -33,7 +54,10 @@ namespace PAREMAUI
                 config.SnackbarConfiguration.ShowTransitionDuration = 500;
                 config.SnackbarConfiguration.SnackbarVariant = MudBlazor.Variant.Filled;
             });
-            return builder.Build();
+
+            var host = builder.Build();
+
+            return host;
         }
     }
 }
