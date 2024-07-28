429b337e4ccc2b4fdac596f5f54aa55ec814d7ed
Cristian C. Giraldo <cristiansiempre.net@hotmail.com>
2024-07-27 22:26:32 -0500

    Agregar archivos de proyecto.

---
+++ PAREMAUI/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+
+namespace PAREMAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.BottomCenter;
+
+                config.SnackbarConfiguration.PreventDuplicates = true;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 4000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = MudBlazor.Variant.Filled;
+            });
+            return builder.Build();
+        }
+    }
+}
