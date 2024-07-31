d090404842172ba7729c48685ee691f668c5a010
Cristian C. Giraldo <cristiansiempre.net@hotmail.com>
2024-07-30 20:36:15 -0500

    Prefijo `tw-` y ajustes de configuración Se agrego en la configuracion de tailwind un prefijo para evitar conflictos con Mudblazor

--- PAREMAUI/tailwind.config.js
+++ PAREMAUI/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    prefix: 'tw-',
+    fontFamily: {
+        sans: ['Jost','Roboto', 'sans-serif'],
+    },
     content: [
         "./**/*.{razor,html,cshtml}",
         "./node_modules/flowbite/**/*.js"
